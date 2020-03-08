5e037cd09ca8d7686038f2a8a14a1410f577952d
Carlos Marín <carlosmmarq@ext.inditex.com>
2020-03-08 22:58:17 +0100

    Añadida redux devtool extension al store

--- src/store.js
+++ src/store.js
@@ -3,11 +3,9 @@ import player from "./Application/components/Player/reducers";
 import login from "./Application/components/Login/reducers";
 
 export default createStore(
-  combineReducers(
-    {
-      player,
-      login
-    },
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  combineReducers({
+    player,
+    login
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
